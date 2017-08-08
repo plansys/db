b069aa413aef3b2b76bda2a3a0725cc8b1ff383d
Naufal <bosnaufalemail@gmail.com>
2017-08-08 17:54:24 +0700

    fix base init

--- src/Init.php
+++ src/Init.php
@@ -37,12 +37,8 @@ class Init
     public static function getBase($host)
     {
         return [
-            'pages' => [
-                '' => [
-                    'dir'=> realpath(dirname(__FILE__) . '/..') . '/pages',
-                    'url' => $host . '/pages/'
-                ]
-            ]
+            'dir'=> realpath(dirname(__FILE__) . '/..') . '/pages',
+            'url' => $host . '/pages/'
         ];
     }
 }
